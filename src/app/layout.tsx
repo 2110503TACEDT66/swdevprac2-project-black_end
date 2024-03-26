66c70d4aa47923c633e921399b01eabe34e54003
goffu7 <135923234+goffu7@users.noreply.github.com>
2024-03-27 00:10:36 +0700

    footet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
 import NextAuthProvider from '@/NextAuthProvider'
 import ReduxProvider from '@/redux/ReduxProvider'
+import Footer from '@/components/Footer'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -28,6 +29,7 @@ export default async function RootLayout({
           <NextAuthProvider session={nextAuthSession} >
             <TopMenu/>
               {children}
+            <Footer/>
           </NextAuthProvider>
         </ReduxProvider>
       </body>
--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -4,12 +4,14 @@ import { useDispatch } from "react-redux"
 import { useSession } from "next-auth/react"
 import getInterview from "@/libs/getInterview"
 import getInterviews from "@/libs/getInterviews"
+import deleteInterview from "@/libs/deleteInterview"
+import { Button } from "@mui/material"
 
 export default async function BookingList({user, interviewJson} : {user:userJSON, interviewJson:Promise<BookingJson>}){
     
     var interviewsJsonReady = await interviewJson;
     if(!interviewsJsonReady){
-        interviewsJsonReady = await getInterviews(user.data.token);
+        interviewsJsonReady = await getInterviews(user?.data.token);
     }
     if(!interviewsJsonReady){
         throw new Error('Failed to fetch Interviews')
@@ -22,9 +24,18 @@ export default async function BookingList({user, interviewJson} : {user:userJSON
             <div className="m-20 flex flex-row items-center justify-around flex-wrap p-10 ">
             {
                 interviewsJsonReady.data.map((BookingItem)=>(
-                    <div>
-                        {BookingItem._id}
-                    </div>  
+                    <div className="items-center text-center w-full h-full">
+                        <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2 rounded-lg flex flex-row text-left w-3/5">
+                            <div className="m-5">
+                            <div className="text-sm m-2">Name : {BookingItem.user}</div>
+                            <div className="text-sm m-2">Citizen ID : {BookingItem._id} </div>
+                            </div>
+                            <div className="m-5">
+                            <div className="text-sm m-2">Selected Company :  {BookingItem.company.name} </div>
+                            <div className="text-sm m-2">Booking Date : {BookingItem.interviewDate} </div>
+                            </div>
+                            </div> 
+                        </div> 
                 ))
             }
             </div>
@@ -40,14 +51,14 @@ export default async function BookingList({user, interviewJson} : {user:userJSON
         //         Cancel Booking
         //         </button>
                 
-        //         <div className="m-5">
-        //         <div className="text-sm m-2">Name : {BookingItem.name}</div>
-        //         <div className="text-sm m-2">Citizen ID : {BookingItem.id} </div>
-        //         </div>
-        //         <div className="m-5">
-        //         <div className="text-sm m-2">Selected Company :  {BookingItem.company} </div>
-        //         <div className="text-sm m-2">Booking Date : {BookingItem.interviewDate} </div>
-        //         </div>
+                // <div className="m-5">
+                // <div className="text-sm m-2">Name : {BookingItem.name}</div>
+                // <div className="text-sm m-2">Citizen ID : {BookingItem.id} </div>
+                // </div>
+                // <div className="m-5">
+                // <div className="text-sm m-2">Selected Company :  {BookingItem.company} </div>
+                // <div className="text-sm m-2">Booking Date : {BookingItem.interviewDate} </div>
+                // </div>
         //         </>:''
         //     }
         //     {
